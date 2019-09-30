e41f8ed2e0cb2e5921911fd6442791e33b9390a0
midasdev711 <midasdev711@gmail.com>
2019-09-30 21:04:18 +0300

    do some updates in the signup route

--- src/app.js
+++ src/app.js
@@ -3,9 +3,7 @@ import {
   BrowserRouter, Switch, Route, Redirect,
 } from 'react-router-dom'
 import { Formik, Form } from 'formik'
-import {
-  AppSchema, appInitialValues,
-} from './utils/formik'
+import { AppSchema } from './utils/formik'
 import './styles/app.css'
 import Layout from './layout/layout.jsx'
 import SignUp from './auth/signUp.jsx'
@@ -18,36 +16,62 @@ function App() {
     <div className="app">
       <BrowserRouter>
         <Route
-          render={({ history }) => (
-            <Formik
-              initialValues={appInitialValues}
-              validationSchema={AppSchema}
-              // validate={validate}
-              onSubmit={(values, { setSubmitting }) => {
-                console.log('onSubmit values', values)
-                // const { education, ...restValues } = values
-                history.push('/result/success')
-                // JSON.stringify(values, null, 2)
-                // setSubmitting(false)
-              }}
-              render={(props) => {
-                console.log('App formik props', props)
-                return (
-                  <Form>
-                    <Layout>
-                      <Switch>
-                        <Route path="/signup" component={SignUp} />
-                        <Route path="/upload/medical-license" component={UploadMedicalLicense} />
-                        <Route path="/update/profile" component={UpdateProfile} />
-                        <Route path="/result/success" component={SuccessResult} />
-                        <Redirect to="/signup" />
-                      </Switch>
-                    </Layout>
-                  </Form>
-                )
-              }}
-            />
-          )}
+          render={({ match, history }) => {
+            const {
+              params: {
+                fullName = 'Ivan Pavlov',
+                email = 'ivan.pavlov@gmail.com',
+              },
+            } = match
+
+            const initialValues = {
+              fullName,
+              email,
+              phonePrefix: '234',
+              phone: '',
+              password: '',
+              medicalLicense: null,
+              avatar: '',
+              skills: [],
+              educations: [],
+              education: {
+                qualification: '',
+                school: '',
+                graduatedAt: '',
+              },
+            }
+
+            return (
+              <Formik
+                initialValues={initialValues}
+                validationSchema={AppSchema}
+                // validate={validate}
+                onSubmit={(values, { setSubmitting }) => {
+                  console.log('onSubmit values', values)
+                  // const { education, ...restValues } = values
+                  history.push('/result/success')
+                  // JSON.stringify(values, null, 2)
+                  // setSubmitting(false)
+                }}
+                render={(props) => {
+                  console.log('App formik props', props)
+                  return (
+                    <Form>
+                      <Layout>
+                        <Switch>
+                          <Route path="/signup" component={SignUp} />
+                          <Route path="/upload/medical-license" component={UploadMedicalLicense} />
+                          <Route path="/update/profile" component={UpdateProfile} />
+                          <Route path="/result/success" component={SuccessResult} />
+                          <Redirect to="/signup" />
+                        </Switch>
+                      </Layout>
+                    </Form>
+                  )
+                }}
+              />
+            )
+          }}
         />
       </BrowserRouter>
     </div>
--- src/auth/signUpForm.jsx
+++ src/auth/signUpForm.jsx
@@ -1,23 +1,78 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { withRouter } from 'react-router-dom'
-import { connect, Field } from 'formik'
+import { connect, Field, ErrorMessage } from 'formik'
 import Button from 'antd/es/button'
 import Typography from 'antd/es/typography'
+import Select from 'antd/es/select'
+import Input from 'antd/es/input'
 import { triggerFormLevelValidation } from '../utils/formik.js'
 import FieldLabel from '../partials/fieldLabel.jsx'
 import CustomInputComponent from '../partials/formik/customInputComponent.jsx'
 import '../styles/signUpForm.scss'
 
 const { Paragraph } = Typography
+const { Option } = Select
 
 function SignUpForm({ history, formik }) {
+  const signUp = () => {
+    triggerFormLevelValidation(formik, async () => {
+      // console.log('signUp', formik.values)
+      const {
+        fullName: name, email, phonePrefix, phone, password,
+      } = formik.values
 
-  function signUp() {
-    triggerFormLevelValidation(formik, () => (
-      history.push('/upload/medical-license')
-    ))
+      const url = 'https://hooks.slack.com/services/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'
+      const payload = {
+        name,
+        email,
+        phone: `${phonePrefix}${phone}`,
+        password,
+        
+        channel: '#physicianlanding',
+        username: 'Physician SIgnup Bot',
+        text: `A physician named ${name} signed up to view patient records. his info is as follows: "Name, email, phone, password", "icon_emoji": ":trophy:"`,
+      }
+
+      try {
+        const response = await fetch(url, {
+          method: 'POST',
+          body: JSON.stringify(payload),
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        })
+        const json = await response.json()
+        console.log('Success:', JSON.stringify(json))
+        history.push('/upload/medical-license')
+      } catch (error) {
+        console.error('Error:', error)
+      }
+    })
   }
+
+  const prefixSelector = (
+    <Field
+      name="phonePrefix"
+      render={({ field, form }) => (
+        <Select
+          {...field}
+          style={{ width: 80 }}
+          onChange={(value) => {
+            form.setFieldValue('phonePrefix', value)
+          }}
+        >
+          <Option value="234">+234</Option>
+          <Option value="971">+971</Option>
+          <Option value="65">+65</Option>
+          <Option value="1">+1</Option>
+          <Option value="91">+91</Option>
+          <Option value="880">+880</Option>
+          <Option value="93">+93</Option>
+        </Select>
+      )}
+    />
+  )
 
   return (
     <div className="sign-up-form">
@@ -36,7 +91,16 @@ function SignUpForm({ history, formik }) {
       <FieldLabel label="Phone Number">
         <Field
           name="phone"
-          component={CustomInputComponent}
+          render={({ field }) => (
+            <React.Fragment>
+              <Input
+                style={{ width: '100%' }}
+                addonBefore={prefixSelector}
+                {...field}
+              />
+              <ErrorMessage {...field} />
+            </React.Fragment>
+          )}
         />
       </FieldLabel>
       <FieldLabel label="Password">
@@ -53,7 +117,7 @@ function SignUpForm({ history, formik }) {
       </div>
       <div className="sign-up-form-terms-conditions">
         <Paragraph>
-          By clicking "Claim My Account", I agree to the 
+          By clicking "Claim My Account", I agree to the
           <a href="https://alemhealth.com/termsofservice"> Terms {'&'} Conditions</a> of signing up.
         </Paragraph>
       </div>
